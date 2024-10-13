6f7f187e11a905ad211e4f7e72b6343346d7c08d
Andi Lavera <49383564+AndiLavera@users.noreply.github.com>
2024-10-13 17:48:16 -0400

    🚨 Comment stdarch_x86_mm_shuffle feature

--- surge-math/src/lib.rs
+++ surge-math/src/lib.rs
@@ -3,50 +3,54 @@
 #![allow(unused_imports)]
 #![feature(test)]
 #![feature(core_intrinsics)]
-#![feature(stdarch_x86_mm_shuffle)]
+// #[cfg(target_arch = "x86_64")]
+// #![feature(stdarch_x86_mm_shuffle)]
 #![feature(trait_alias)]
 
-#[macro_use] mod imports; use imports::*;
-#[macro_use] pub mod m128;
+#[macro_use]
+mod imports;
+use imports::*;
+#[macro_use]
+pub mod m128;
 
-x!{align}
-x!{error}
-x!{fill}
-x!{allpass}
-x!{basic_ops}
-x!{clamp}
-x!{clear}
-x!{convert}
-x!{copy}
-x!{correlated_noise}
-x!{encode}
-x!{get_sign}
-x!{absmax}
-x!{access}
-x!{mul}
-x!{sub}
-x!{sum}
-x!{add}
-x!{accum}
-x!{fast}
-x!{flops}
-x!{hardclip}
-x!{interp}
-x!{limit_range}
-x!{ord}
-x!{padding}
-x!{pan}
-x!{saturate}
-x!{scan}
-x!{scratch}
-x!{simd}
-x!{sinc}
-x!{sine}
-x!{softclip}
-x!{square}
-x!{tanh}
-x!{trixpan}
-x!{utility}
-x!{value_cast}
-x!{white_noise}
-x!{window}
+x! {align}
+x! {error}
+x! {fill}
+x! {allpass}
+x! {basic_ops}
+x! {clamp}
+x! {clear}
+x! {convert}
+x! {copy}
+x! {correlated_noise}
+x! {encode}
+x! {get_sign}
+x! {absmax}
+x! {access}
+x! {mul}
+x! {sub}
+x! {sum}
+x! {add}
+x! {accum}
+x! {fast}
+x! {flops}
+x! {hardclip}
+x! {interp}
+x! {limit_range}
+x! {ord}
+x! {padding}
+x! {pan}
+x! {saturate}
+x! {scan}
+x! {scratch}
+x! {simd}
+x! {sinc}
+x! {sine}
+x! {softclip}
+x! {square}
+x! {tanh}
+x! {trixpan}
+x! {utility}
+x! {value_cast}
+x! {white_noise}
+x! {window}
